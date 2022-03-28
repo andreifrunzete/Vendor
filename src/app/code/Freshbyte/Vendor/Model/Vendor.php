24718af14dc57b1b4e84d70b00cba85a02361b31
andrei <andreifrunzete01@gmail.com>
2022-03-28 15:07:32 +0300

    Create vendor Portal that will open a containing grid

--- src/app/code/Freshbyte/Vendor/Model/Vendor.php
+++ src/app/code/Freshbyte/Vendor/Model/Vendor.php
@@ -9,8 +9,8 @@ class Vendor extends \Magento\Framework\Model\AbstractModel
     protected function _construct()
     {
         $this->_init(\Bss\NewEntityType\Model\ResourceModel\Vendor::class);
-        $this->_init('Vendors', 'ID');
-        $this->_init('Vendors_details', 'id');
+        $this->_init('vendors', 'ID');
+        $this->_init('vendors_address', 'id');
     }
 
 }
