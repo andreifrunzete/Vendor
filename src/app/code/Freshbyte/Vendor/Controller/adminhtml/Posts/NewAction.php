ce8ca9b7f60b8734c68845f7304e81fccc6982c0
andrei <andreifrunzete01@gmail.com>
2022-04-04 16:50:44 +0300

    Vendor Details Page V1.0

--- src/app/code/Freshbyte/Vendor/Controller/adminhtml/Posts/NewAction.php
+++ src/app/code/Freshbyte/Vendor/Controller/adminhtml/Posts/NewAction.php
@@ -2,16 +2,39 @@
 namespace Freshbyte\Vendor\Controller\adminhtml\Posts;
 
 use Freshbyte\Vendor\Controller\adminhtml\Posts;
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
 
 class NewAction extends Posts
 {
+
+    protected $resultPageFactory;
+
     /**
-     * Create new news action
+     * Constructor
      *
-     * @return void
+     * @param \Magento\Backend\App\Action\Context        $context
+     * @param \Magento\Framework\View\Result\PageFactory $resultPageFactory
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory
+    ) {
+        $this->resultPageFactory = $resultPageFactory;
+        parent::__construct($context);
+    }
+
+
+
+    /**
+     * Posts action
+     *
+     * @return \Magento\Framework\Controller\ResultInterface
      */
     public function execute()
     {
-        $this->_forward('edit');
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->getConfig()->getTitle()->prepend(__("Vendor Details"));
+        return $resultPage;
     }
 }
--- src/app/code/Freshbyte/Vendor/Model/ResourceModel/Vendor/Collection.php
+++ src/app/code/Freshbyte/Vendor/Model/ResourceModel/Vendor/Collection.php
@@ -2,8 +2,16 @@
 
 namespace Freshbyte\Vendor\Model\ResourceModel\Vendor;
 
-class Collection extends \Magento\Eav\Model\Entity\Collection\AbstractCollection
+use Magento\Framework\Data\Collection\Db\FetchStrategyInterface as FetchStrategy;
+use Magento\Framework\Data\Collection\EntityFactoryInterface as EntityFactory;
+use Magento\Framework\Event\ManagerInterface as EventManager;
+use Psr\Log\LoggerInterface as Logger;
+
+class Collection extends \Magento\Framework\View\Element\UiComponent\DataProvider\SearchResult
 {
+    protected $_idFieldName = 'id';
+    protected $_eventPrefix = 'freshbyte_vendor_post_collection';
+    protected $_eventObject = 'post_collection';
 
     protected function _construct()
     {
